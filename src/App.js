0fe0f315660ae65dfabef48ee657ac22bc6fa382
Sergio Hortua <sergioandresjsv@gmail.com>
2020-10-26 19:34:17 -0500

    instalacion de package styled-component y react-spring

--- src/App.js
+++ src/App.js
@@ -1,4 +1,44 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Modal } from './components/Modal';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Button = styled.button`
+  min-width: 100px;
+  padding: 16px 32px;
+  border-radius: 4px;
+  border: none;
+  background: #141414;
+  color: #fff;
+  font-size: 24px;
+  cursor: pointer;
+`;
+
+function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(prev => !prev);
+  };
+
+  return (
+    <>
+      <Container>
+        <Button onClick={openModal}>I'm a modal</Button>
+        <Modal showModal={showModal} setShowModal={setShowModal} />
+      </Container>
+    </>
+  );
+}
+
+export default App; 
+/*import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -22,4 +62,4 @@ function App() {
   );
 }
 
-export default App;
+export default App; */
